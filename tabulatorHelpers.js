951b5b4e46ea504fdc39032c0f119f4e55084b72
databind10 <90654757+databind10@users.noreply.github.com>
2021-09-13 17:58:02 -0500

    Create tabulatorHelpers.js

---
+++ tabulatorHelpers.js
@@ -0,0 +1,7 @@
+// tabulator v4.9.3 Helpers
+// version 1.0.0 Build: 1
+// © Databind, 2021
+// https://github.com/databind10
+//
+// Released under GNU GENERAL PUBLIC LICENSE
+// =====================================================================================================================
